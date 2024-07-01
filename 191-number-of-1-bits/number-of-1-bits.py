fe4a9ca72c581ef3655a26b4b4f2dadf01b76a92
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2024-07-01 10:58:08 -0500

    Time: 40 ms (25.77%) | Memory: 16.6 MB (25.49%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,10 +1,16 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        ctr=0
-        a=bin(n).replace("0b", "")
-        for i in a:
-            if (i=='1'):
-                ctr+=1
-        return ctr
+        # ctr=0
+        # a=bin(n).replace("0b", "")
+        # for i in a:
+        #     if (i=='1'):
+        #         ctr+=1
+        # return ctr
+        res=0
+        while n:
+            res+=n%2
+            n=n>>1
+
+        return res
 
         
