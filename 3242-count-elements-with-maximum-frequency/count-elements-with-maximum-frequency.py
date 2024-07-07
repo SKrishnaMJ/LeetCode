6cd7a6bf12f646bd049b525069b045859ecdb5d9
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2024-07-07 17:49:08 -0500

    Time: 40 ms (76.59%) | Memory: 16.6 MB (6.24%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        my_dict={}
+        ans=[]
+        prev=0
+        res=0
+        ctr=0
+        for num in nums:
+            if num in my_dict:
+                my_dict[num]+=1
+            else:
+                my_dict[num] = 1
+        for k,v in my_dict.items():
+            ans.append(v)
+        maximum = max(ans)
+        for i in sorted(ans,reverse=True):
+            if i==maximum and maximum==1:
+                ctr+=1
+            elif i==maximum:
+                res+=i
+            else:
+                continue
+        if(ctr==len(ans)):
+            return ctr
+        else:
+            return res
+
+        
