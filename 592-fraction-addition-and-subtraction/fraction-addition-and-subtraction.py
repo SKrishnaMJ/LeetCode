7dddeb1b2c053494797f6e2124d5591daf504676
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2024-08-26 17:52:57 -0500

    Time: 34 ms (70.06%) | Memory: 16.7 MB (18.43%) - LeetSync

---
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.py
@@ -0,0 +1,22 @@
+from fractions import Fraction
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        ex=list(expression)
+        num=ex[0]
+        l=[]
+        ans=0
+        for i in range(1, len(ex)):
+            if ex[i] == "-" or ex[i] == "+":
+                l.append(num)
+                num=""
+                num+=ex[i]
+            else:
+                num+=ex[i]
+        l.append(num)
+        for j in range(len(l)):
+            ans+=eval(l[j])
+        res=str(Fraction(ans).limit_denominator(3000))
+        if res in ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "-1", "-2", "-3", "-4", "-5","6", "-7", "-8", "-9"]:
+            return res+"/1"
+        return res
+        
