74371ae2298e14a42fa3e88252a7051b062514f0
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2024-09-25 14:28:13 -0500

    Time: 105 ms (82.00%) | Memory: 19.1 MB (18.46%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,17 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        my_dict={}
-        ans=0
-        for i in nums:
-            if (i in my_dict.keys()):
-                my_dict[i]+=1
-            else:
-                my_dict[i]=1
-        for k,v in my_dict.items():
-            if(v==1):
-                ans=k
-                break
-        return ans
-            
-
+        res=0
+        for num in nums:
+            res^=num
+        return res
         
