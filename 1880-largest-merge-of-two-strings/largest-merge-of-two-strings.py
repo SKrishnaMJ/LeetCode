d34f9c848fac533923f30bf7eed7b9165712cab3
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2024-12-02 16:42:30 -0600

    Time: 39 ms (63.12%) | Memory: 17.5 MB (10.13%) - LeetSync

--- 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.py
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.py
@@ -1,40 +1,15 @@
-# class Solution:
-#     def largestMerge(self, word1: str, word2: str) -> str:
-#         merge=""
-#         p1,p2=0,0
-#         while(p1<len(word1) and p2<len(word2)):
-#             if word1[p1]>word2[p2] or (word1[p1]==word2[p2] and word1[p1:]>word2[p2:]):
-#                 merge+=word1[p1]
-#                 p1+=1
-#             elif word1[p1]<word2[p2] or (word1[p1]==word2[p2] and word1[p1:]<word2[p2:]):
-#                 merge+=word2[p2]
-#                 p2+=1
-#             elif (word1[p1:]==word2[p2:]):
-#                 merge+=word1[p1:]+word2[p2:]
-#                 p1+=len(word1[p1:])
-#                 p2+=len(word2[p2:])
-#                 break
-
-#         if p1<len(word1):
-#             merge+=word1[p1:]
-#         else:
-#             merge+=word2[p2:]
-#         return merge
 class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
         merge = []
         p1, p2 = 0, 0
-
         while p1 < len(word1) and p2 < len(word2):
-            # Compare substrings lexicographically
+            
             if word1[p1:] > word2[p2:]:
                 merge.append(word1[p1])
                 p1 += 1
             else:
                 merge.append(word2[p2])
                 p2 += 1
-
-        # Append the remainder of the strings
         if p1 < len(word1):
             merge.append(word1[p1:])
         else:
