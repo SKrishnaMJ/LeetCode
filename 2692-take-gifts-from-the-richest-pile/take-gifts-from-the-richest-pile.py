c51a28bae7c4f8c47b2fb7f5ab578c6761f841b4
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2024-12-12 15:52:42 -0600

    Time: 5 ms (52.95%) | Memory: 17.4 MB (22.10%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -1,7 +1,9 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts=[-g for g in gifts]
+        heapq.heapify(gifts)
         for i in range(k):
-            num = max(gifts)
-            gifts.remove(num)
-            gifts.append(int(num**0.5))
-        return sum(gifts)
+            n=-heapq.heappop(gifts)
+            n**=0.5
+            heapq.heappush(gifts,int(-n))
+        return -sum(gifts)
