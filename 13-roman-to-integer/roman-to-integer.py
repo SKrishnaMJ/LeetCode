a92fff43f4d115dc60ed5798015b7e91ae3ebce7
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-02-21 11:28:47 -0600

    Time: 3 ms (80.48%) | Memory: 18 MB (20.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,17 +1,17 @@
-class Solution(object):
-    def romanToInt(self, s):
-        my_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        prev = 0
-        total = 0
-        for i in reversed(s):
-            val = my_dict[i]
-            if(val<prev):
-                total-=val
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        my_dict={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        res=0
+        n=len(s)
+        i=0
+        while(i<n):
+            if  i<n-1 and my_dict[s[i]] < my_dict[s[i+1]]:
+                res+=my_dict[s[i+1]]-my_dict[s[i]]
+                i+=2
             else:
-                total+=val
-            prev=val
-        return total
+                res+=my_dict[s[i]]
+                i+=1
+        return res
 
 
-        
         
