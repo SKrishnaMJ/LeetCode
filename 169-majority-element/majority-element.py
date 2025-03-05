73d31c617bc7d583b601736239519d6cf4bd14f8
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-03-05 11:00:39 -0600

    Time: 9 ms (34.60%) | Memory: 19.3 MB (87.31%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        my_dict = {}
-        for i in nums:
-            if i in my_dict:
-                my_dict[i] += 1
+        # count = dict(Counter(nums))
+        # c=-1
+        # ans=0
+        # for k,v in count.items():
+        #     if v>c:
+        #         c=v
+        #         ans=k
+        # return ans
+        
+        # Optimal solution
+        ans,c=0,0
+        for num in nums:
+            if c==0:
+                ans=num
+            if ans==num:
+                c+=1
             else:
-                my_dict[i] = 1
-        for k,v in my_dict.items():
-            if(v > len(nums)/2):
-                return k
+                c-=1
+        return ans
