b8a4674726b9ddd04fcf642cbe2444fc6551e7cd
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-03-04 21:27:43 -0600

    Time: 14 ms (79.43%) | Memory: 17.8 MB (82.36%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,20 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ctr=0
-        my_dict1=Counter(ransomNote)
-        my_dict2=Counter(magazine)
-        # print(my_dict1)
-        for k,v in my_dict1.items():
-            if k not in my_dict2.keys() or v>my_dict2[k]:
+        mag = dict(Counter(magazine))
+        ran = dict(Counter(ransomNote))
+        for i in ransomNote:
+            if i not in mag or mag[i]<ran[i]:
                 return False
         return True
-        #     if k in my_dict2.keys() and v<=my_dict2[k]:
-        #         ctr+=1
-        # if ctr>0:
-        #     return True
-        # else:
-        #     return False
-
-
 
         
