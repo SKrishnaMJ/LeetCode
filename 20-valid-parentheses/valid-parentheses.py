83c9abda82a01b7544bbcc3da0b56ef20a36fc5e
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-03-08 12:35:56 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,18 @@
-class Solution(object):
-    def isValid(self, s):
-        my_dict = {')':'(', ']':'[','}':'{'}
-        stack =[]
-        for char in s:
-            if char in my_dict.values():
-                stack.append(char)
-            elif char in my_dict.keys():
-                if not stack or my_dict[char] != stack.pop():
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)==1:
+            return False
+        st=[]
+        my_dict = {')':'(', '}':'{',']':'['}
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                st.append(i)
+            else:
+                if not st or st.pop()!=my_dict[i]:
                     return False
-        return not stack
-        
+        if len(st)!=0:
+            return False
+        else:
+            return True
 
         
