056f7247adf0a5ca19a4a980775f47da09a52e31
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-04-02 08:51:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (19.20%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -1,22 +1,30 @@
+# class Solution:
+#     def maximumTripletValue(self, nums: List[int]) -> int:
+#         # ans = float("-inf")
+#         # for i in range(len(nums)):
+#         #     j=i+1
+#         #     k=len(nums)-1
+#         #     while j<k:
+#         #         res = (nums[i]-nums[j])*nums[k]
+#         #         ans = max(ans, res)
+#         #         if nums[j]>=nums[k]:
+#         #             k-=1
+#         #         else:
+#         #             j+=1
+#         # return ans if ans>=0 else 0
+#         ans = float("-inf")
+#         for i in range(len(nums)-2):
+#             for j in range(i+1,len(nums)-1):
+#                 for k in range(j+1,len(nums)):
+#                     res = (nums[i]-nums[j])*nums[k]
+#                     ans = max(ans, res)
+#         return ans if ans>=0 else 0
+                    
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        # ans = float("-inf")
-        # for i in range(len(nums)):
-        #     j=i+1
-        #     k=len(nums)-1
-        #     while j<k:
-        #         res = (nums[i]-nums[j])*nums[k]
-        #         ans = max(ans, res)
-        #         if nums[j]>=nums[k]:
-        #             k-=1
-        #         else:
-        #             j+=1
-        # return ans if ans>=0 else 0
-        ans = float("-inf")
-        for i in range(len(nums)-2):
-            for j in range(i+1,len(nums)-1):
-                for k in range(j+1,len(nums)):
-                    res = (nums[i]-nums[j])*nums[k]
-                    ans = max(ans, res)
-        return ans if ans>=0 else 0
-                    
+        hi, d, ans = 0, 0, 0
+        for x in nums:
+            ans = max(ans, d*x)
+            d = max(hi - x, d)
+            hi = max(hi, x)
+        return ans
