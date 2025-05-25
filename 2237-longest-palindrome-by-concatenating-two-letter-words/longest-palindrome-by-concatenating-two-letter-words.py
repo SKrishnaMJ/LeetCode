9484dcfbcec0176cd54df86b1cacbd1f5e42da2d
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-05-25 17:12:25 -0500

    Time: 55 ms (64.70%) | Memory: 35.7 MB (12.54%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,7 +1,6 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         my_dict = dict(Counter(words))
-        print(my_dict)
         pairs, central = 0,0
         for word, cnt in my_dict.items():
             rev = word[::-1]
