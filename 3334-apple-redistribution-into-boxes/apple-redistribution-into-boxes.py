34f2c645a7157f93ccab7952b07b51b4059f556a
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-06-04 19:20:00 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (13.08%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
@@ -2,12 +2,11 @@ class Solution:
     def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
         tot = sum(apple)
         capacity.sort(reverse=True)
-        c=0
-        for i in capacity:
-            if tot<=0:
-                break
-            tot-=i
-            c+=1
-            
-        return c
+        box=0
+        for c in capacity:
+            if tot>0:
+                tot-=c
+                box+=1
+        return box
+
         
