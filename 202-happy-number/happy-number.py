5aa26a77d6f4954425f74160867f9c9e33d459a6
Sai Krishna M J <146481712+SKrishnaMJ@users.noreply.github.com>
2025-06-07 13:57:37 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (17.67%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,16 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        
+        while n>5:
+            summ=0
+            while n!=0:
+                rem = n%10
+                n = n//10
+                summ+=(rem*rem)
+            n = summ
         if n==1:
             return True
-        ans=0
-
-        while(int(n)>5):
-            n=str(n)
-            for i in range(len(n)):
-                ans+=int(n[i])*int(n[i])
-            if ans==1:
-                return True
-            n=str(ans)
-            ans=0
-        return False
+        else:
+            return False
         
